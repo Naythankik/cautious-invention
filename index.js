189d87c76624bff5af24e18fa7daca80ac8068c2
Naythankik <nathabolarin@gmail.com>
2024-03-07 10:32:37 +0100

    WIP

--- index.js
+++ index.js
@@ -38,3 +38,36 @@ window.addEventListener('resize', (e) => {
     }
 
 }, true);
+
+
+// Logic for image carousel
+
+const spanButton = document.querySelector('.span-buttons')
+const serviceImage = document.querySelectorAll('.service-image');
+
+for (const button of spanButton.children) {
+    button.addEventListener('click', (e) => {
+        Array.from(spanButton.children).filter((btn) => btn.classList.contains('active-span') ? btn.classList.remove('active-span') : '');
+        
+        const index = e.currentTarget;
+        index.classList.add('active-span');
+
+        for (let ind = 0; ind < serviceImage.length; ind++) {
+            if(ind == (index.id -1)){
+                if(serviceImage[ind].classList.contains('hidden')){
+                    serviceImage[ind].classList.remove('hidden')
+                    serviceImage[ind].classList.add('active')
+                }else{
+                    serviceImage[ind].classList.add('active')
+                }
+            }else{
+                if(serviceImage[ind].classList.contains('active')){
+                    serviceImage[ind].classList.remove('active')
+                    serviceImage[ind].classList.add('hidden')
+                }else{
+                     serviceImage[ind].classList.add('hidden')
+                }
+            }
+        }
+    })
+}
